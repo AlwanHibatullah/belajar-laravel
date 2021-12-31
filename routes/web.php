99663ed23ab4bb95d0d5eaf7c10118c387f216c7
Alwan Hibatullah <alwanhibatullah@gmail.com>
2021-12-31 20:38:50 +0700

    Belajar routes dan view

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return view('home', [
+        'title' => 'Home'
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        'title' => 'About',
+        'name' => 'Alwan Hibatullah',
+        'email' => 'alwanhibatullah@gmail.com',
+        'image' => 'poto.jpg'
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts', [
+        'title' => 'Posts'
+    ]);
 });
